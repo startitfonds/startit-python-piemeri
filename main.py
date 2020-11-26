6c199e80ca702e816ff5fe6fe2f96caa147a8590
skmara2 <skmara2@inbox.lv>
2020-11-26 15:56:08 +0200

    audio

--- main.py
+++ main.py
@@ -3,28 +3,26 @@ from src.audio_mix import atskano, mikse_skanas, saglaba_skanu
 
 # 1. solis
 # Pārbaudām vai bibliotēka strādā
-# parbaudam_darbibu()
+#parbaudam_darbibu()
 
 # 2.solis
 # Atrodam un saglabājam audio datni wav formātā
 # Audio datnes var atrast, piemēram, šeit - http://soundbible.com/
-# atskano_datni("clock-chimes-daniel_simon.wav")
+atskano_datni("poker-chips-daniel_simon.wav")
 
 # 3.solis
 # Veidojam skaņas tīri matemātiski
-# beep(261.626, 1)   # DO
-# beep(294.33, 1)    # RE
-# beep(327.03, 1)    # MI
-# beep(348.83, 1)    # FA
-# beep(392.44, 1)    # SOL
-# beep(436.04, 1)    # LA
-# beep(490.55, 1)    # SI
-# beep(523.25, 1)    # DO
-# beep()             # Noklusētā frekvence un ilgums
+#beep(327.03, 1)    # MI
+#beep(348.83, 1)    # FA
+#beep(392.44, 1)    # SOL
+#beep(436.04, 1)    # LA
+#beep(490.55, 1)    # SI
+#beep(523.25, 1)    # DO
+#beep()             # Noklusētā frekvence un ilgums
 
 # 4.solis
 # Miksējam skaņas, mainām skaļumu
-# atskano("dati/clock-chimes-daniel_simon.wav")
+#atskano("Birds In Forest-SoundBible.com-472460759.wav")
 
 # 5.solis
 # saglabajam skaņu datnē
