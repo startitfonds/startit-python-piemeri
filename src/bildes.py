ad97b7274a987b2ac37e6e936d7f4fc9ab4c7231
Gundega Dekena <gundega.dekena@gmail.com>
2021-01-21 23:19:50 +0200

    add papildus piemeri

--- src/bildes.py
+++ src/bildes.py
@@ -2,6 +2,8 @@ import sys
 import os
 from PIL import Image
 
+def cartoon(pix):
+    return pix if pix > 100 else 250                    
 
 def bilzu_info(mape, atvert=False):
     datnes = os.listdir(mape)
@@ -10,7 +12,8 @@ def bilzu_info(mape, atvert=False):
             with Image.open(os.path.join(mape,infile)) as im:
                 print(infile, im.format, f"{im.size}x{im.mode}")
                 if atvert:
-                    im.show()
+                    im2 = Image.eval(im, cartoon)
+                    im2.show()
         except OSError:
             pass
 
@@ -27,3 +30,15 @@ def sikteli(mape):
                     im.save(os.path.join(mape,outfile), im.format)
             except OSError:
                 print("cannot create thumbnail for", infile)
+
+
+def bilde():
+    from PIL import Image
+    datne = "bilde.jpg"
+    izmers = (100, 100)
+    with Image.open(datne) as im:
+        print(datne, im.format, f"{im.size}x{im.mode}")
+        im.show()
+        im.thumbnail(izmers)
+        im.save("bilde-maza.jpg", im.format)
+        im.show()
